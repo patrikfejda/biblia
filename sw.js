dc7cfdd517487165ba1c1cd202b50ea52ede6640
Patrik Fejda <patrikfejda@gmail.com>
2023-10-13 14:00:24 +0200

    test

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
 self.addEventListener('install', (e) => {
     e.waitUntil(
-        caches.open('biblia-cache').then((cache) => cache.addAll([
-            '/biblia/',
-            '/biblia/index.html',
-            '/biblia/index.js',
-            '/biblia/bible.js',
-            '/biblia/public/icon.png',
-            'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
+        caches.open('fox-store').then((cache) => cache.addAll([
+            '/packlist/',
+            '/packlist/index.html',
+            '/packlist/index.js',
         ])),
     );
 });
